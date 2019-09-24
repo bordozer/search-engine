d310bbbc684db358cdcbd202b96eaaa012eaa43d
bordozer <bordozer@gmail.com>
2019-09-24 19:19:46 +0300

    WIP document 6

--- src/main/java/com/bordozer/searchengine/converter/DocumentConverter.java
+++ src/main/java/com/bordozer/searchengine/converter/DocumentConverter.java
@@ -4,6 +4,8 @@ import com.bordozer.searchengine.dto.DocumentDto;
 import com.bordozer.searchengine.entity.DocumentEntity;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Objects;
+
 @Slf4j
 public final class DocumentConverter {
 
@@ -11,6 +13,7 @@ public final class DocumentConverter {
     }
 
     public static DocumentEntity toEntity(final DocumentDto dto) {
+        Objects.requireNonNull(dto, "Dto must nit be null");
         final DocumentEntity constructed = new DocumentEntity();
         constructed.setKey(dto.getKey());
         constructed.setContent(dto.getContent());
@@ -18,6 +21,7 @@ public final class DocumentConverter {
     }
 
     public static DocumentDto toDto(final DocumentEntity entity) {
+        Objects.requireNonNull(entity, "Entity must nit be null");
         return DocumentDto.builder()
                 .key(entity.getKey())
                 .content(entity.getContent())
