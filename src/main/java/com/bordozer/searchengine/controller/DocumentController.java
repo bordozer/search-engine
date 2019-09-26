5dd6e6343f441d309d53f5731514a217d24587c0
bordozer <bordozer@gmail.com>
2019-09-26 14:31:23 +0300

    WIP search 19 swagger 3

--- src/main/java/com/bordozer/searchengine/controller/DocumentController.java
+++ src/main/java/com/bordozer/searchengine/controller/DocumentController.java
@@ -3,6 +3,10 @@ package com.bordozer.searchengine.controller;
 import com.bordozer.commons.utils.LoggableJson;
 import com.bordozer.searchengine.dto.DocumentDto;
 import com.bordozer.searchengine.service.DocumentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -18,17 +22,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController()
 @RequestMapping("/doc")
 @RequiredArgsConstructor
+@Api(value = "Document controller", tags = "Operations with Documents")
 public class DocumentController {
 
     private final DocumentService documentService;
 
     @GetMapping(path = "/{key}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Get document by key", response = ResponseEntity.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Document successfully retrieved"),
+            @ApiResponse(code = 422, message = "Document cannot be found by key")
+    })
     public ResponseEntity<DocumentDto> findDocumentByKey(@PathVariable final String key) {
         LOGGER.info("Get document by key '{}'", key);
         return ResponseEntity.ok(documentService.findByKey(key));
     }
 
     @PostMapping(path = "/add", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Add new document to the system", response = ResponseEntity.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Document is successfully saved"),
+            @ApiResponse(code = 400, message = "Bad request"),
+            @ApiResponse(code = 417, message = "Cannot create a Document. Document with provided key already exists")
+    })
     public ResponseEntity<DocumentDto> addDocument(@RequestBody final DocumentDto dto) {
         LOGGER.info("Add a new document: {}", LoggableJson.of(dto));
         return ResponseEntity.ok(documentService.addNew(dto));
--- src/main/java/com/bordozer/searchengine/controller/SearchEngineController.java
+++ src/main/java/com/bordozer/searchengine/controller/SearchEngineController.java
@@ -3,6 +3,10 @@ package com.bordozer.searchengine.controller;
 import com.bordozer.commons.utils.LoggableJson;
 import com.bordozer.searchengine.dto.DocumentDto;
 import com.bordozer.searchengine.service.SearchService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -18,11 +22,16 @@ import java.util.List;
 @RestController()
 @RequestMapping("/search")
 @RequiredArgsConstructor
+@Api(value = "Search controller", tags = "Search for documents")
 public class SearchEngineController {
 
     private final SearchService searchService;
 
     @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Search for documents by tokens", response = ResponseEntity.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Search for documents successfully done"),
+    })
     public ResponseEntity<List<DocumentDto>> search(@RequestParam(value = "token") final List<String> tokens) {
         LOGGER.info("About to search documents by tokens '{}'", LoggableJson.of(tokens));
         return ResponseEntity.ok(searchService.find(tokens));
