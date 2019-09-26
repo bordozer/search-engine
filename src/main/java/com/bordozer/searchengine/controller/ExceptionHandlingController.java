e2dbfa2ff60ed76f5ffacc8275c47347daccc2af
bordozer <bordozer@gmail.com>
2019-09-26 11:17:04 +0300

    WIP search 16 tests

--- src/main/java/com/bordozer/searchengine/controller/ExceptionHandlingController.java
+++ src/main/java/com/bordozer/searchengine/controller/ExceptionHandlingController.java
@@ -1,8 +1,11 @@
 package com.bordozer.searchengine.controller;
 
 import com.bordozer.searchengine.dto.ErrorDto;
+import com.bordozer.searchengine.dto.ImmutableErrorDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,11 +19,18 @@ public class ExceptionHandlingController {
 
     @ExceptionHandler({NoSuchElementException.class})
     public final ResponseEntity<ErrorDto> handleNoSuchElementException(final Exception ex) {
-        return new ResponseEntity<>(ErrorDto.builder().message(ex.getMessage()).build(), HttpStatus.UNPROCESSABLE_ENTITY);
+        return handleException(ex, HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
     @ExceptionHandler({EntityExistsException.class})
     public final ResponseEntity<ErrorDto> handleEntityExistsException(final Exception ex) {
-        return new ResponseEntity<>(ErrorDto.builder().message(ex.getMessage()).build(), HttpStatus.EXPECTATION_FAILED);
+        return handleException(ex, HttpStatus.EXPECTATION_FAILED);
+    }
+
+    private ResponseEntity<ErrorDto> handleException(final Exception ex, final HttpStatus status) {
+        final ImmutableErrorDto message = ErrorDto.builder().message(ex.getMessage()).build();
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+        return new ResponseEntity<>(message, headers, status);
     }
 }
--- src/test/java/com/bordozer/searchengine/controller/DocumentControllerTest.java
+++ src/test/java/com/bordozer/searchengine/controller/DocumentControllerTest.java
@@ -1,5 +1,63 @@
 package com.bordozer.searchengine.controller;
 
-class DocumentControllerTest {
+import com.bordozer.commons.testing.endpoint.AbstractEndpointTest;
+import com.bordozer.commons.testing.endpoint.EndpointTestBuilder;
+import com.bordozer.commons.utils.FileUtils;
+import com.bordozer.searchengine.dto.DocumentDto;
+import com.bordozer.searchengine.service.DocumentService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
+import java.util.NoSuchElementException;
+
+import static com.bordozer.commons.testing.endpoint.EndpointTestBuilder.testEndpoint;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(DocumentController.class)
+class DocumentControllerTest extends AbstractEndpointTest {
+
+    private static final String GET_DOC_URL = "/doc/1024";
+    private static final String GET_EXISTING_DOC_EXPECTED_RESPONSE =
+            FileUtils.readSystemResource("tests/DocumentControllerTest/get-existing-doc-expected-response.json");
+    private static final String GET_NOT_EXISTING_DOC_EXPECTED_RESPONSE =
+            FileUtils.readSystemResource("tests/DocumentControllerTest/get-not-existing-doc-expected-response.json");
+
+    @MockBean
+    private DocumentService documentService;
+
+    @Test
+    void shouldGetDocumentIfExists() {
+        // given
+        final DocumentDto response = DocumentDto.builder()
+                .key("1024")
+                .content("one two three")
+                .build();
+        when(documentService.findByKey("1024")).thenReturn(response);
+
+        final EndpointTestBuilder endpointTest = testEndpoint(GET_DOC_URL)
+                .whenRequest()
+                .thenResponseSuccessWithJsonBody(GET_EXISTING_DOC_EXPECTED_RESPONSE);
+        // when
+        getTo(endpointTest);
+    }
+
+    @Test
+    void shouldReturnErrorIfIfDocumentDoesNotExist() {
+        // given
+        when(documentService.findByKey("1024")).thenThrow(new NoSuchElementException("Document with key '1024' not found"));
+
+        final EndpointTestBuilder endpointTest = testEndpoint(GET_DOC_URL)
+                .whenRequest()
+                .thenResponse()
+                .hasContentType(MediaType.APPLICATION_JSON_UTF8)
+                .hasHttpStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+                .hasBodyJson(GET_NOT_EXISTING_DOC_EXPECTED_RESPONSE)
+                .end();
+        // when
+        getTo(endpointTest);
+    }
 }
