2259b54414b64d05e3e777e693770bdf77975cf1
bordozer <bordozer@gmail.com>
2019-09-26 11:36:25 +0300

    WIP search 16 tests 2

--- src/test/java/com/bordozer/searchengine/controller/DocumentControllerTest.java
+++ src/test/java/com/bordozer/searchengine/controller/DocumentControllerTest.java
@@ -4,6 +4,7 @@ import com.bordozer.commons.testing.endpoint.AbstractEndpointTest;
 import com.bordozer.commons.testing.endpoint.EndpointTestBuilder;
 import com.bordozer.commons.utils.FileUtils;
 import com.bordozer.searchengine.dto.DocumentDto;
+import com.bordozer.searchengine.dto.ImmutableDocumentDto;
 import com.bordozer.searchengine.service.DocumentService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,20 +12,30 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import javax.persistence.EntityExistsException;
 import java.util.NoSuchElementException;
 
 import static com.bordozer.commons.testing.endpoint.EndpointTestBuilder.testEndpoint;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 @WebMvcTest(DocumentController.class)
 class DocumentControllerTest extends AbstractEndpointTest {
 
-    private static final String GET_DOC_URL = "/doc/1024";
+    private static final String DOC_KEY = "1024";
+
+    private static final String GET_DOC_URL = "/doc/" + DOC_KEY;
+    private static final String ADD_DOC_URL = "/doc/add";
+
     private static final String GET_EXISTING_DOC_EXPECTED_RESPONSE =
             FileUtils.readSystemResource("tests/DocumentControllerTest/get-existing-doc-expected-response.json");
     private static final String GET_NOT_EXISTING_DOC_EXPECTED_RESPONSE =
             FileUtils.readSystemResource("tests/DocumentControllerTest/get-not-existing-doc-expected-response.json");
+    private static final String CREATE_NEW_DOC_REQUEST =
+            FileUtils.readSystemResource("tests/DocumentControllerTest/create-new-doc-request.json");
+    private static final String CREATE_NEW_DOC_EXPECTED_RESPONSE =
+            FileUtils.readSystemResource("tests/DocumentControllerTest/create-new-doc-expected-response.json");
+    private static final String DOC_ALREADY_EXISTS_EXPECTED_RESPONSE =
+            FileUtils.readSystemResource("tests/DocumentControllerTest/doc-already-exists-expected-response.json");
 
     @MockBean
     private DocumentService documentService;
@@ -33,24 +44,24 @@ class DocumentControllerTest extends AbstractEndpointTest {
     void shouldGetDocumentIfExists() {
         // given
         final DocumentDto response = DocumentDto.builder()
-                .key("1024")
+                .key(DOC_KEY)
                 .content("one two three")
                 .build();
-        when(documentService.findByKey("1024")).thenReturn(response);
+        when(documentService.findByKey(DOC_KEY)).thenReturn(response);
 
-        final EndpointTestBuilder endpointTest = testEndpoint(GET_DOC_URL)
+        final EndpointTestBuilder endpoint = testEndpoint(GET_DOC_URL)
                 .whenRequest()
                 .thenResponseSuccessWithJsonBody(GET_EXISTING_DOC_EXPECTED_RESPONSE);
         // when
-        getTo(endpointTest);
+        getTo(endpoint);
     }
 
     @Test
     void shouldReturnErrorIfIfDocumentDoesNotExist() {
         // given
-        when(documentService.findByKey("1024")).thenThrow(new NoSuchElementException("Document with key '1024' not found"));
+        when(documentService.findByKey(DOC_KEY)).thenThrow(new NoSuchElementException("Document with key '1024' not found"));
 
-        final EndpointTestBuilder endpointTest = testEndpoint(GET_DOC_URL)
+        final EndpointTestBuilder endpoint = testEndpoint(GET_DOC_URL)
                 .whenRequest()
                 .thenResponse()
                 .hasContentType(MediaType.APPLICATION_JSON_UTF8)
@@ -58,6 +69,46 @@ class DocumentControllerTest extends AbstractEndpointTest {
                 .hasBodyJson(GET_NOT_EXISTING_DOC_EXPECTED_RESPONSE)
                 .end();
         // when
-        getTo(endpointTest);
+        getTo(endpoint);
+    }
+
+    @Test
+    void shouldCreateNewDocument() {
+        // given
+        final ImmutableDocumentDto dto = DocumentDto.builder()
+                .key(DOC_KEY)
+                .content("one two three")
+                .build();
+        when(documentService.addNew(dto)).thenReturn(dto);
+
+        final EndpointTestBuilder endpoint = testEndpoint(ADD_DOC_URL)
+                .whenRequest()
+                .withContentType(MediaType.APPLICATION_JSON_UTF8)
+                .withBodyJson(CREATE_NEW_DOC_REQUEST)
+                .thenResponseSuccessWithJsonBody(CREATE_NEW_DOC_EXPECTED_RESPONSE);
+        // when
+        postTo(endpoint);
+    }
+
+    @Test
+    void shouldReturnErrorIfDocumentKeyAlreadyExists() {
+        // given
+        final ImmutableDocumentDto dto = DocumentDto.builder()
+                .key(DOC_KEY)
+                .content("one two three")
+                .build();
+        when(documentService.addNew(dto)).thenThrow(new EntityExistsException("Document with key '1024' already exists"));
+
+        final EndpointTestBuilder endpoint = testEndpoint(ADD_DOC_URL)
+                .whenRequest()
+                .withContentType(MediaType.APPLICATION_JSON_UTF8)
+                .withBodyJson(CREATE_NEW_DOC_REQUEST)
+                .thenResponse()
+                .hasContentType(MediaType.APPLICATION_JSON_UTF8)
+                .hasHttpStatus(HttpStatus.EXPECTATION_FAILED)
+                .hasBodyJson(DOC_ALREADY_EXISTS_EXPECTED_RESPONSE)
+                .end();
+        // when
+        postTo(endpoint);
     }
 }
